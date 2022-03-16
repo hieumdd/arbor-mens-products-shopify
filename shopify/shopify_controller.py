9a3aac81b008b88f9710092b0da949f4f2bb5cf8
HM <hieumdd@gmail.com>
2022-03-16 11:01:35 +0700

    hotfix: Fixed shopify-controller

--- shopify/shopify_controller.py
+++ shopify/shopify_controller.py
@@ -6,7 +6,7 @@ from shopify import shopify_service
 def shopify_controller(body: dict[str, Any]) -> dict[str, Union[str, int]]:
     return shopify_service.pipeline_service(
         shopify_service.pipelines[body["resource"]],
-        shopify_service.shops[body["shop_url"]],
+        shopify_service.shops[body["shop"]],
         body.get("start"),
         body.get("end"),
     )
